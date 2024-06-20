2309e3b94115ca867b11abfa38737fbc3f6f5ddd
gbrown-n <gbrown.hm@gmail.com>
2024-06-20 10:04:57 +1000

    new images, color palette and service cards

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
+    "name": "Eleven Drums",
+    "email": "vonbottrell@outlook.com",
     "phoneForTel": "555-779-4407",
     "phoneFormatted": "(555) 779-4407",
     "address": {
