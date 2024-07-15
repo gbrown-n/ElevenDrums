41273424176c091d12b8d84a2e80a1ae228f31f0
gbrown-n <gbrown.hm@gmail.com>
2024-07-16 09:37:46 +1000

    changed email

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,6 +1,6 @@
 module.exports = {
     "name": "Eleven Drums",
-    "email": "gbrown.hm@gmail.com",
+    "email": "vonbottrell@outlook.com",
     "phoneForTel": "555-779-4407",
     "phoneFormatted": "(555) 779-4407",
     "address": {
