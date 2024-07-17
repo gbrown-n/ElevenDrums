37b2cc3a3b8e2e4d8a3e3a2c930756b610b1382b
gbrown-n <gbrown.hm@gmail.com>
2024-07-17 15:02:56 +1000

    Reviews added

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,20 @@
 module.exports = {
     "name": "Eleven Drums",
     "email": "vonbottrell@outlook.com",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "phoneForTel": "0400207936",
+    "phoneFormatted": "04 0020 7936",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "lineOne": "Tallegalla",
+        "lineTwo": "",
+        "city": "",
+        "state": "",
+        "zip": "",
+        "mapLink": ""
     },
     "socials": {
         "facebook": "https://www.facebook.com/profile.php?id=100090474027547",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.elevendrums.com.au"
 };
